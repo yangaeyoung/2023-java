eee73451c9742d8bbc2e7dcfff01cfd37c9f5b94
aeyoung <aeyoung0325@naver.com>
2024-01-29 12:20:59 +0900

    24. 01. 29.

--- src/main/java/com/green/day84/StreamEx5.java
+++ src/main/java/com/green/day84/StreamEx5.java
@@ -2,8 +2,10 @@ package com.green.day84;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
 
+// 책 826? 쪽 참고
 public class StreamEx5 {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(new Integer[]{100, 1, 8, 10, 22});
@@ -12,9 +14,11 @@ public class StreamEx5 {
         int sum = intList.stream().mapToInt(item -> item).sum();
         System.out.println("sum: " + sum);
 
-        OptionalDouble avg = intList.stream().mapToInt(item -> item).average(); //OptionalDouble: null이 들어가지 않음
+        OptionalDouble avg = intList.stream().mapToInt(item -> item).average(); // OptionalDouble: null이 들어가지 않음
         if(avg.isPresent()){ // null check
             System.out.println("avg: " + avg.getAsDouble());
         }
+        double avg2 = Optional.ofNullable(avg).map(item -> item.getAsDouble()).orElse(0.0);
+        System.out.println("avg2: " + avg2);
     }
 }
